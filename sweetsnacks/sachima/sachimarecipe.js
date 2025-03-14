8a3bb81b1e69274f389e9cfdd72114ab50f347d1
stambong24 <tambong.sophia24@gmail.com>
2025-03-14 13:15:35 -0400

    Add files via upload

---
+++ sweetsnacks/sachima/sachimarecipe.js
@@ -0,0 +1,69 @@
+$(".slide-1").hide()
+$(".slide-2").hide()
+$("#menu-screen").hide()
+$("#return").hide()
+$("#recipe-1").hide()
+$("#recipe-2").hide()
+
+
+$(".intro-screen").click(function(){
+    $(".intro-screen").toggleClass("active");
+    $(".intro-screen").fadeOut(1500);
+    $("#menu-screen").fadeIn(5000);
+    $("#return").fadeIn(5000);
+
+});
+
+/*----MENU BUTTONS-----*/
+
+$("#sachima-7-big").click(function(){
+
+    $("#menu-screen").hide();
+    $(".slide-2").fadeIn()
+  
+
+})
+
+$("#sachima-6-big").click(function(){
+
+    $("#menu-screen").hide();
+    $(".slide-1").fadeIn()
+  
+
+})
+
+$(".return-menu").click(function(){
+
+    $("#menu-screen").fadeIn();
+    $(".slide-2").hide();
+    $(".slide-1").hide();
+   
+
+  
+
+})
+
+
+
+/*----SPECIALITY BUTTONS-----*/
+
+$("#specialty-1").click(function(){
+
+
+    $("#instructions").hide();
+    $("#recipe-2").hide();
+    $("#recipe-1").fadeIn(300);
+
+
+})
+
+$("#specialty-2").click(function(){
+
+
+    $("#instructions").hide();
+    $("#recipe-1").hide();
+    $("#recipe-2").fadeIn(300);
+
+    
+
+})
